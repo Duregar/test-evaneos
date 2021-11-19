9ed30b37d8410894c10f3ec69bc4f157fc21ae4b
Raphaël Aurières <raphael.aurieres@gmail.com>
2021-11-19 17:06:29 +0100

    Use Composer autoloading.

--- example/example.php
+++ example/example.php
@@ -6,20 +6,6 @@ use Evaneos\Factory\TemplateManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/../src/Entity/Destination.php';
-require_once __DIR__ . '/../src/Entity/Quote.php';
-require_once __DIR__ . '/../src/Entity/Site.php';
-require_once __DIR__ . '/../src/Entity/Template.php';
-require_once __DIR__ . '/../src/Entity/User.php';
-require_once __DIR__ . '/../src/Factory/TemplateManagerFactory.php';
-require_once __DIR__ . '/../src/Helper/SingletonTrait.php';
-require_once __DIR__ . '/../src/Context/ApplicationContext.php';
-require_once __DIR__ . '/../src/Repository/Repository.php';
-require_once __DIR__ . '/../src/Repository/DestinationRepository.php';
-require_once __DIR__ . '/../src/Repository/QuoteRepository.php';
-require_once __DIR__ . '/../src/Repository/SiteRepository.php';
-require_once __DIR__ . '/../src/TemplateManager.php';
-
 $faker = \Faker\Factory::create();
 
 $template = new Template(
