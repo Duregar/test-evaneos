3b1cb1b3eda28e4b00e2cdb7a7b1f2e540f2ab63
Raphaël Aurières <raphael.aurieres@gmail.com>
2021-11-19 15:59:51 +0100

    Remove unnecessary tests.

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -33,37 +33,10 @@ class TemplateManager
             $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
             $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
 
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            }
-
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
-            }
-
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
-        }
-
-        if (isset($destination)) {
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
-        } else {
-            $text = str_replace('[quote:destination_link]', '', $text);
+            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destinationOfQuote->countryName . '/quote/' . $_quoteFromRepository->id, $text);
+            $text = str_replace('[quote:summary_html]', Quote::renderHtml($_quoteFromRepository), $text);
+            $text = str_replace('[quote:summary]', Quote::renderText($_quoteFromRepository), $text);
+            $text = str_replace('[quote:destination_name]', $destinationOfQuote->countryName,$text);
         }
 
         /*
@@ -71,9 +44,7 @@ class TemplateManager
          * [user:*]
          */
         $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $this->currentUser;
-        if($_user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($_user->firstname)), $text);
-        }
+        $text = str_replace('[user:first_name]', ucfirst(mb_strtolower($_user->firstname)), $text);
 
         return $text;
     }
