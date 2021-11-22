f8d8a805a6ecc1b85be3e297ed304019698a6ffe
Raphaël Aurières <raphael.aurieres@gmail.com>
2021-11-22 12:10:20 +0100

    Remove useless code.

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -2,12 +2,8 @@
 
 namespace Evaneos;
 
-use Evaneos\Entity\Quote;
 use Evaneos\Entity\Template;
-use Evaneos\Entity\User;
 use Evaneos\Formatter\FormatterInterface;
-use Evaneos\Repository\DestinationRepository;
-use Evaneos\Repository\SiteRepository;
 
 class TemplateManager
 {
@@ -46,16 +42,5 @@ class TemplateManager
                 $formatter->format($template, $data);
             }
         }
-    }
-
-    /**
-     * @param Template $template
-     * @param string $tag
-     * @param string $replacement
-     */
-    protected function replaceTag(Template $template, $tag, $replacement)
-    {
-        $template->subject = str_replace("[$tag]", $replacement, $template->subject);
-        $template->content = str_replace("[$tag]", $replacement, $template->content);
     }
 }
